061b2b4e3e6fd2abbe9612b309b25ca10bf1f561
Mackio0 <asiamaman000@gmail.com>
2024-12-31 01:53:11 +0630

    Assets: Change asset names to kabab-case

---
+++ src/app/components/Logo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "../../assets/logo.png";
+import Image from "next/image";
+
+const Logo = ({ className }) => {
+  console.log(logo.url);
+  return (
+    <>
+      <Image src={logo} alt="Moana Logo" className={className} />
+    </>
+  );
+};
+
+export default Logo;
